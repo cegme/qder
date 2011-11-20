c975bb8e1a23a36fd7838a3150b52fd61bce554e
Christan Grant <christangrant@gmail.com>
2011-11-20 01:10:39 -0500

    finished the cgrant_bf_init function

--- bloomfilter/bf.sql
+++ bloomfilter/bf.sql
@@ -2,24 +2,35 @@
 -- Ideas of this implementation of a bloom filter taken from
 -- http://antognini.ch/papers/BloomFilters20080620.pdf
 
-CREATE OR REPLACE TYPE cgrant_bf AS (
-	-- array svec or regular array
-	-- array size
-	-- k number of hash functions
-	-- array with hash function seeds <-- This could be created by the system
-	-- 
+-- DROP TYPE cgrant_bf;
+CREATE TYPE cgrant_bf AS (
+	vec boolean[], -- array svec or regular array
+	m INT, -- array size
+	k INT, -- k number of hash functions
+	seeds integer[] -- array with hash function seeds
 );
 
+DROP FUNCTION cgrant_bf_init(m INT, k INT);
 CREATE OR REPLACE FUNCTION cgrant_bf_init(m INT, k INT) RETURNS cgrant_bf AS 
 $$
-DECLARE
-	m_array = -- Make an m-dimentional array with all FALSE values
-	k_seeds = -- Add an array of k random seed vals
+DECLARE thearray boolean[];
+DECLARE thehash integer[];
+DECLARE bloomfilter cgrant_bf;
 BEGIN
-	RETURN cgrant_bf(m_array, m, k, k_seeds);
+	SELECT ARRAY(SELECT FALSE FROM generate_series(0,m))::boolean[] INTO thearray; 
+	SELECT ARRAY(SELECT generate_series(0,k))::integer[] INTO thehash;
+	bloomfilter := ROW(
+		thearray,
+		m,
+		k,
+		thehash
+	);
+	RETURN bloomfilter;
 END;
 $$ LANGUAGE plpgsql;
 
 
-CREATE OR REPLACE FUNCTION cgrant_bf_add_value() -- add value to the bf
-CREATE OR REPLACE FUNCTION cgrant_bf_contains() -- contains
+-- CREATE OR REPLACE FUNCTION cgrant_bf_add_value() -- add value to the bf
+-- CREATE OR REPLACE FUNCTION cgrant_bf_contains() -- contains
+
+
