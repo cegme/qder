156ff5190b42136efdbd3b5a1b0190963df90268
Christan Grant <christangrant@gmail.com>
2011-11-20 23:45:44 -0500

    created the initial version of the bloom filter code. Testing needed

--- bloomfilter/bf.sql
+++ bloomfilter/bf.sql
@@ -10,7 +10,7 @@ CREATE TYPE cgrant_bf AS (
 	seeds integer[] -- array with hash function seeds
 );
 
-DROP FUNCTION cgrant_bf_init(m INT, k INT);
+-- DROP FUNCTION cgrant_bf_init(m INT, k INT);
 CREATE OR REPLACE FUNCTION cgrant_bf_init(m INT, k INT) RETURNS cgrant_bf AS 
 $$
 DECLARE thearray boolean[];
@@ -27,10 +27,63 @@ BEGIN
 	);
 	RETURN bloomfilter;
 END;
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql IMMUTABLE;
 
 
--- CREATE OR REPLACE FUNCTION cgrant_bf_add_value() -- add value to the bf
--- CREATE OR REPLACE FUNCTION cgrant_bf_contains() -- contains
+/*DROP FUNCTION cgrant_hash1(text);
+CREATE FUNCTION cgrant_hash1(s text) RETURNS integer AS
+$$
+	return hash(s)
+$$ LANGUAGE plpythonu;
+*/
+
+-- hashpjw
+DROP FUNCTION cgrant_hash2 (text);
+CREATE FUNCTION cgrant_hash2(s text) RETURNS integer AS
+$$
+	val = 0
+	for c in s:
+		val = (val << 4) + ord(c)
+		tmp = val & 0xf0000000
+		if tmp != 0:
+			val = val ^ (tmp >> 24)
+			val = val ^ tmp
+	return val
+$$ LANGUAGE plpythonu;
+
+
+DROP FUNCTION cgrant_hashindices(text, int, int);
+CREATE FUNCTION cgrant_hashindices(s text, k int, m int) RETURNS integer[] AS
+$$
+	indices = []
+	rv = plpy.execute("SELECT g, cgrant_hash2($1) AS h2 FROM generate_series(1,$2) AS g", [s, k+1 ])
+	for i in xrange(1, k + 1):
+		indices.append((hash(s) + i * rv['h2']) % m)
+	return indices
+$$ LANGUAGE plpythonu;
+
+
+-- Insert values into the hash function
+-- HT to http://www.coolsnap.net/kevin/?p=13
+-- h_i(val) = (h1(val) + i * h2(val)) % m
+CREATE OR REPLACE FUNCTION cgrant_bf_add_value(s text, bf cgrant_bf) 
+RETURNS cgrant_bf AS
+$$
+	rv = plpy.execute("SELECT cgrant_hasindices($1, $2, $3)", [s, bf['k'], bf['m']])
+	bf['vec'] = map(lambda x: True if x[0] in rv[0] else x[1], enumerate(bf['vec']))
+	return bf
+-- add value to the bf
+$$ LANGUAGE plpythonu;
+
+
+CREATE OR REPLACE FUNCTION cgrant_bf_contains(s text, bf cgrant_bf)
+RETURNS boolean AS
+$$
+
+	rv = plpy.execute("SELECT cgrant_hasindices($1, $2, $3)", [s, bf['k'], bf['m']])
+	return all(rv[0])
+$$ LANGUAGE plpythonu;
+
+
 
 
