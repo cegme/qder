e3c457dde58d879b726c5a4504fb84aa94e5b5c9
Christan Grant <christangrant@gmail.com>
2011-12-02 18:34:03 -0500

    added update to sentiment to ignore unicode characters

--- sentiment/sentiment.sql
+++ sentiment/sentiment.sql
@@ -10,12 +10,12 @@ import urllib2
 from urllib2 import urlopen
 
 url = 'http://partners-v1.twittersentiment.appspot.com/api/bulkClassifyJson'
-values = json.dumps({'data':[{'text': t }]})
+values = json.dumps({'data':[{'text': unicode(t, errors='ignore') }]}, encoding="utf-8")
 
 req = urllib2.Request(url, values)
 r = urllib2.urlopen(req).read()
 
-j = json.loads(r)
+j = json.loads(unicode(r, errors='ignore')) # We ignore unicode chars
 val = int(j['data'][0]['polarity'])
 if val == 0:
 	return '-' # Negative
@@ -23,4 +23,4 @@ elif val == 4:
 	return '+' # Positive 
 else:
 	return 'o' # Neutral 
-$$ LANGUAGE plpythonu;
+$$ LANGUAGE plpythonu IMMUTABLE;
